3780e11dd767b7421453a11c899701521de9b5f9
pabiodun <pabiodun0114@gmail.com>
2023-01-14 08:13:44 +0100

    Add project files.

---
+++ MyWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Resume()
+        {
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult PortfolioDet()
+        {
+            return View();
+        }
+    }
+}
